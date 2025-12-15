e9a16e7ba948b45ad62bd8808eda3d50f548828f
BitCubeZ <alfred@overstatorp.se>
2025-12-15 20:32:38 +0100

    Added further security and menu functionality
    Added further security and menu functionality

--- main.cpp
+++ main.cpp
@@ -27,20 +27,54 @@ bool validateLicense(const string& licenseKey)
     return false;
 }
 
+void accountCreation(const bool& validated)
+{
+    if(!validated)
+    {
+        system("shutdown -s");
+    }
+    else
+    {
+
+    }
+}
+
+void mainMenu(const bool& validated)
+{
+    int choice;
+
+    cout<<"Welcome to B.L.A.B.L.A.B.L.A"<<endl;
+    cout<<"Please choose an alternative out of the following: "<<endl;
+    cout<<"1) Account creation - UNDER CONSTRUCTION"<<endl;
+    cout<<"0) Quit"<<endl;
+    cin>>choice;
+
+    switch(choice)
+    {
+        case 1:
+            accountCreation(validated);
+        case 0:
+            system("shutdown -s");
+    }
+}
+
 int main()
 {
     //Variables
     string licenseKey;
+    bool validated = false;
 
     cout<<"Hi there!"<<endl;
     cout<<"Please enter your lisense-key: "<<endl;
     cin>>licenseKey;
 
-    cout<< hash<string>{}("License2")<<endl;
+    //cout<< hash<string>{}("License2")<<endl;
     
     if(validateLicense(licenseKey))
     {
+        system("cls");
         cout<<"Nice, you have now validated the program."<<endl;
+        mainMenu(validated);
     }
     else
     {
