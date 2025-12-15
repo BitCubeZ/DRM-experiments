1e0b883edfccfde904ac50327bf46da586e3951d
AGamesFrTho <alfred@overstatorp.se>
2025-12-15 01:27:34 +0100

    Basic functionality
    Introducing basic functionality.

---
+++ main.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <functional>
+using namespace std;
+
+size_t hashLicense(const string& key)
+{
+    return hash<string>{}(key);
+}
+
+const std::vector<size_t> validHashes = 
+{
+    3319241314578178735,
+    3319242414089806946
+};
+
+bool validateLicense(const string& licenseKey)
+{
+    size_t inputHash = hashLicense(licenseKey);
+
+    for (size_t validHash : validHashes)
+    {
+        if (inputHash == validHash)
+            return true;
+    }
+    return false;
+}
+
+int main()
+{
+    //Variables
+    string licenseKey;
+
+    cout<<"Hi there!"<<endl;
+    cout<<"Please enter your lisense-key: "<<endl;
+    cin>>licenseKey;
+
+    cout<< hash<string>{}("License2")<<endl;
+    
+    if(validateLicense(licenseKey))
+    {
+        cout<<"Nice"<<endl;
+    }
+    else
+    {
+        cout<<"Not okay"<<endl;
+    }
+
+    return 0;
+}
